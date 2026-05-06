c5a6b53a959b0bf24300de80349854552fa006b7
Gerald Benjamin Huanto Ayma <231443@unsaac.edu.pe>
2026-05-06 16:02:54 -0500

    Implementacion de una mejora de formulario de contacto

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function saludar() {
         document.getElementById("resultado").innerText = "Hola " + nombre + ", bienvenido al sistema.";
     }
 }
+
+// Parte F - Nueva función de validación
+function validarCorreo() {
+    // Aquí buscamos el ID "correo" que pusimos en el HTML
+    let correoElemento = document.getElementById("correo");
+    
+    if (!correoElemento) {
+        console.error("No se encontró el elemento con ID 'correo'");
+        return;
+    }
+
+    let correo = correoElemento.value;
+
+    if (correo === "") {
+        document.getElementById("mensajeCorreo").innerText = "Ingrese un correo electrónico."; [cite= 439]
+    } else {
+        document.getElementById("mensajeCorreo").innerText = "Correo registrado correctamente."; [cite= 442]
+    }
+}
